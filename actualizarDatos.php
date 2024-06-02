5ca913040f447629e5c255ff6cb972ea1b9a3876
Alfredo Gomez <alfretg.c@hotmail.com>
2024-06-02 15:07:18 -0500

    Mejoramiento en la interfaz y seguridad en los roles en el modulo de restablecer password estudiante

--- actualizarDatos.php
+++ actualizarDatos.php
@@ -4,12 +4,12 @@ session_start();
 //si existe la sesion
 if (isset($_SESSION['usuarios'])) {
     $correo = $_SESSION['usuarios'];
-    
+
     //Hacemos la conexion a la base de datos
     require 'conexion/conexion.php';
 
     //Hacemos la consulta para traer los datos del usuario
-    $statement = $conexion->prepare('SELECT id, nombres_completos, estados_usuarios_id, correo FROM usuarios WHERE correo = :correo LIMIT 1');
+    $statement = $conexion->prepare('SELECT id, nombres_completos, estados_usuarios_id, roles_id, correo FROM usuarios WHERE correo = :correo LIMIT 1');
     $statement->execute(array(':correo' => $correo));
     $resultado = $statement->fetch();
 
@@ -21,61 +21,107 @@ if (isset($_SESSION['usuarios'])) {
         $estados_usuarios_id = $resultado['estados_usuarios_id'];
     }
 
-    //Hacemos la consulta para traer los grados
-    $statementGrado = $conexion->prepare('SELECT * FROM grados');
-    $statementGrado->execute();
-    $resultadoGrado = $statementGrado->fetchAll();
+    try {
+        // Recibimos los datos
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nuevaPassword = $_POST['nueva_password'];
+            $confirmarPassword = $_POST['confirmar_password'];
 
-    //Recibimos los datos del formulario
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nombre = $_POST['nombre'];
-        $password = $_POST['password'];
-        $confipassword = $_POST['confipassword'];
-        $grado = isset($_POST['grado']) ? $grado = $_POST['grado'] : $grado = null;
+            $errores = '';
 
-        $errores = '';
-        $success = '';
-        //Validamos los datos
-        if (empty($nombre) or empty($password) or empty($confipassword) or empty($grado)) {
-            $errores .= "<script> Swal.fire(
-                        'Opps...',
-                        'Por favor rellena todos los datos correctamente',
-                        'error')</script>";
-            $errores .= '<li class="#ef5350 red lighten-1">Por favor rellena todos los datos correctamente</li>';
-        } else {
+            // Verificamos que el captcha este correcto
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $captcha = $_POST['g-recaptcha-response'];
+            $secretkey = "6LfL90kgAAAAADdvH6SJ4K8Kb-7ho9JzExaKi-P7";
 
-            //Encriptamos la contraseña
-            $password = hash('sha512', $password);
-            $confipassword = hash('sha512', $confipassword);
+            $respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
+            $atributos = json_decode($respuesta, true);
 
-            if ($password != $confipassword) {
+            if ($atributos['success'] == false) {
                 $errores .= "<script> Swal.fire(
-                            'Opps...',
-                            'Las contraseñas no son iguales',
-                            'error')</script>";
-                $errores .= '<li class="#ef5350 red lighten-1">Las contraseñas no son iguales</li>';
+                                'Por favor',
+                                'Valida el reCaptcha',
+                                'error')</script>";
+                $errores .= '<li class="#ef5350 red lighten-1">Por favor verifica el captcha</li>';
+            }
+
+            // Verificamos que los campos no esten vacios
+            if (empty($nuevaPassword) || empty($confirmarPassword) || empty($captcha)) {
+                $errores .= "<script> Swal.fire(
+                                'Por favor',
+                                'Rellena todos los campos',
+                                'error')</script>";
+                $errores .= '<li class="#ef5350 red lighten-1">Por favor rellena todos los campos</li>';
+            } else {
+                $nuevaPassword = hash('sha512', $nuevaPassword);
+                $confirmarPassword = hash('sha512', $confirmarPassword);
+
+                // Verificamos que las contraseñas coincidan
+                if ($nuevaPassword != $confirmarPassword) {
+                    $errores .= "<script> Swal.fire(
+                                'Opps...',
+                                'Las contraseñas no coinciden',
+                                'error')</script>";
+                    $errores .= '<li class="#ef5350 red lighten-1">Las contraseñas no coinciden</li>';
+                }
+
+
+                // Actualizar la contraseña del usuario
+                $statement = $conexion->prepare('UPDATE usuarios SET password = :password WHERE correo = :correo');
+                $statement->execute(array(':password' => $nuevaPassword, ':correo' => $correo));
+
+                // Enviar correo electrónico notificando el cambio de contraseña
+                $subject = "Cambio de Contraseña Exitoso";
+                $message = "
+                            <html>
+                            <head>
+                                <title>Cambio de Contraseña Exitoso</title>
+                            </head>
+                            <body style='font-family: Arial, sans-serif;'>
+                                <div style='background-color: #6A80C0; color: #fff; padding: 10px; text-align: center;'>
+                                    <h2>Cambio de Contraseña Exitoso</h2>
+                                </div>
+                                <div style='padding: 20px;'>
+                                    <p>Su contraseña ha sido cambiada con éxito.</p>
+                                    <p>Si usted no realizó este cambio, por favor contacte con nuestro soporte técnico.</p>
+                                </div>
+                            </body>
+                            </html>
+                        ";
+
+                // El encabezado del correo
+                $headers = "From: no-reply@educasen.com\r\n";
+                $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+                // Envía el correo electrónico
+                mail($correo, $subject, $message, $headers);
+
+                $errores .= "<script> Swal.fire({
+                            title: 'Cambio de Contraseña',
+                            text: 'Su contraseña ha sido cambiada con éxito.',
+                            icon: 'success',
+                            confirmButtonText: 'OK'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = 'cerrar.php'; // Redirige a la página de login.php
+                            }
+                        });
+                        </script>";
             }
         }
-
-        //Si no hay errores actualizamos los datos
-        if ($errores == '') {
-            $statement = $conexion->prepare('UPDATE usuarios SET nombres_completos = :nombre, password = :password, grado_id = :grado WHERE id = :id');
-            $statement->execute(array(':nombre' => $nombre, ':password' => $password, ':grado' => $grado, ':id' => $id));
-            $success .= "<script> Swal.fire(
-                        'Usuario actualizado',
-                        'Inicie sesion con los nuevos datos',
-                        'success')</script>";
-            $success .= '<li class="#00e676 green accent-3">Usuario actualizado, inicie sesion con los nuevos datos</li>';
-            $_SESSION['usuarios'] = $correo;
-
-            //Esperar 0.2 segundos destruir sesion y redireccionar
-            header("Refresh:0.4; url=actualizar.php");
-        }
+    } catch (Exception $e) {
+        // Manejo de excepciones
+        $errores .= "Error: " . $e->getMessage();
+        // También puedes guardar esto en un archivo de registro
+        error_log("Error en restablecer la contraseña " . $e->getMessage(), 0);
     }
-    
-require 'views/actualizarDatos.view.php';
+
+    if ($resultado['roles_id'] == 2) { // Si es estudiante
+        require 'views/actualizarDatos.view.php';
+    } else if ($resultado['roles_id'] == 1) { // Si es administrador
+        header('Location: admin/dashboard.php');
+    }
+
 } else {
     header('Location: login.php');
 }
-
-?>
--- views/actualizarDatos.view.php
+++ views/actualizarDatos.view.php
@@ -1,165 +1,145 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="views/imagenes/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="views/estilos/actualizarInfo.css">
-  <title>Actualizar informacion</title>
+  <title>Actualizar contraseña</title>
 
   <!--Import materialize.css-->
-  <link type="text/css" rel="stylesheet" href="views/materialize/css/materialize.min.css"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="views/materialize/css/materialize.min.css" media="screen,projection" />
   <!-- Libreria sweetalert -->
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
-<body>
-<!------------------------------------------------------------------------------------------------------------> 
-<!-- Cabecera menu -->
-<nav role="navigation">
-  <div class="nav-wrapper">
-    <a href="" class="brand-logo" >
-      <img src="views/imagenes/logoIECentral-removebg.png" alt="logoIECentral" class="logoIECentral">
-    </a>
 
-    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fa-solid fa-bars"></i></a>
+<body>
+  <!------------------------------------------------------------------------------------------------------------>
+  <!-- Cabecera menu -->
+  <nav role="navigation">
+    <div class="nav-wrapper">
+      <a href="" class="brand-logo">
+        <img src="views/imagenes/logoIECentral-removebg.png" alt="logoIECentral" class="logoIECentral">
+      </a>
+
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fa-solid fa-bars"></i></a>
       <ul class="right hide-on-med-and-down">
         <li><a class="sidenav-close" href="menuPrincipal.php">Materias</a></li>
         <li><a class="sidenav-close" href="#"><?php echo 'Usuario: ' .  $nombres_completos; ?></a>
-            <ul>
-                <li><a class="sidenav-close" href="#">Ayuda</a></li>
-                <li><a class="sidenav-close" href="cerrar.php">Cerrar sesion</a></li>
-              </ul>
-        </li>
-        <li><a class="sidenav-close" href="perfil.php">Regresar</a></li>
-      </ul>
-  </div>
-</nav>
-
-<!-- Cabecera menus para pantallas pequeñas  -->
-<ul class="sidenav" id="mobile-demo">
-    <img class="" src="views/imagenes/file.png" width="300" height="120" alt="logoICFES">
-    <li><a class="sidenav-close" href="menuPrincipal.php">Materias</a></li>
-    <li><a class="sidenav-close" href="#"><?php echo 'Usuario: ' .  $nombres_completos; ?></a>
-        <ul>
+          <ul>
             <li><a class="sidenav-close" href="#">Ayuda</a></li>
-            <li><a class="sidenav-close" href="perfil.php">Regresar</a></li>
-            <li><a class="sidenav-close" href="cerrar.php">Cerrar sesion</a></li>
-        </ul>
-    </li>
+            <li><a class="sidenav-close" href="cerrar.php">Cerrar sesión</a></li>
+          </ul>
+        </li>
+        <li><a class="sidenav-close" href="ajustes.php">Regresar</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Cabecera menus para pantallas pequeñas  -->
+  <ul class="sidenav" id="mobile-demo">
     <div class="logoMobile">
       <img src="views/imagenes/favicon.svg" class="logoMobile" alt="educasen" width="200" height="200">
     </div>
-</ul>
-<!------------------------------------------------------------------------------------------------------------> 
+    <li><a class="sidenav-close" href="menuPrincipal.php">Materias</a></li>
+    <li><a class="sidenav-close" href="#"><?php echo 'Usuario: ' .  $nombres_completos; ?></a>
+      <ul>
+        <li><a class="sidenav-close" href="#">Ayuda</a></li>
+        <li><a class="sidenav-close" href="ajustes.php">Regresar</a></li>
+        <li><a class="sidenav-close" href="cerrar.php">Cerrar sesión</a></li>
+      </ul>
+    </li>
+  </ul>
+  <!------------------------------------------------------------------------------------------------------------>
 
-<div class="contenedor-card">
-  <div class="card-registro">
+  <div class="contenedor-card">
+    <div class="card-registro">
 
-    <h1 class="tituloCard">Actualizar datos</h1>
-    <figure class="fondoLading">
-        <lottie-player class="fondoLading__lottie" src="https://assets9.lottiefiles.com/packages/lf20_md7jx0xq.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
-    </figure>
+      <h1 class="tituloCard">Actualizar contraseña</h1>
+      <figure class="fondoLading">
+        <lottie-player class="fondoLading__lottie" src="https://assets9.lottiefiles.com/packages/lf20_md7jx0xq.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+      </figure>
 
-    <!-- Formulario de registro -->
-    <form class="col s12" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" name="actualizar">
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="name" type="text" name="nombre" class="validate" minlength="10" required value="<?php echo $nombres_completos; ?>">
-          <label for="name">Nombre</label>
+      <!-- Formulario de registro -->
+      <form class="col s12" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" name="actualizar">
+        <div class="row">
+          <br>
+          <div class="row__input input-field col s6">
+            <input id="password" type="password" name="nueva_password" minlength="7" class="validate" required>
+            <label for="password">Contraseña</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="confiPassword" type="password" name="confirmar_password" minlength="7" class="validate" required>
+            <label for="confiPassword">Confirmar contraseña</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="password" type="password" name="password" minlength="7" class="validate" required>
-          <label for="password">Contraseña</label>
-        </div>
-      
-        <div class="input-field col s6">
-          <input id="confiPassword" type="password" name="confipassword" minlength="7" class="validate" required>
-          <label for="confiPassword">Confirmar contraseña</label>
-        </div>
-      </div>
 
-      <div class="row">  
-        <label>Seleccione el grado</label>
-        <select class="browser-default" name="grado" required>
-          <option value="" disabled selected>Seleccione el grado</option>
-          <?php
-              // Se recorre el array de grados
-              foreach($resultadoGrado as $gradoCurso) {
-                echo '<option value="' . $gradoCurso['id'] . '">' . $gradoCurso['nombre_grado'] . '</option>';
-              }
-          ?>
-        </select>
-      </div>
-      <br>
-
-      <!-- recaptcha -->
-      <div>
-        <div class="g-recaptcha" data-sitekey="6LfL90kgAAAAAESzVF-LUvSIl6RNVx13O3MsOD49">
+        <!-- recaptcha -->
+        <div class="contenedor__recaptcha">
+          <div class="g-recaptcha" data-sitekey="6LfL90kgAAAAAESzVF-LUvSIl6RNVx13O3MsOD49"></div>
         </div>
-      </div>
-      <br>
+        <br>
 
-      <div class=" row offset-s1 center-align">
-      <i class="#7986cb indigo lighten-2 btn " onclick="actualizar.submit()">Actualizar</i>
-      </div>
-
-      <?php if(!empty($errores)): ?> <!-- Si hay errores los muestra -->
-        <div class="error">
-          <ul>
-            <?php echo $errores; ?>
-          </ul>
+        <div class=" row offset-s1 center-align">
+          <i class="#7986cb indigo lighten-2 btn " onclick="actualizar.submit()">Actualizar</i>
         </div>
-        <?php elseif(!empty($success)): ?> <!-- Si no hay errores y si hay un mensaje de exito -->
+
+        <?php if (!empty($errores)) : ?> <!-- Si hay errores los muestra -->
+          <div class="error">
+            <ul>
+              <?php echo $errores; ?>
+            </ul>
+          </div>
+        <?php elseif (!empty($success)) : ?> <!-- Si no hay errores y si hay un mensaje de exito -->
           <div class="success">
             <ul>
               <?php echo $success; ?>
             </ul>
           </div>
-      <?php endif; ?>
-    </form>
-  </div>
-</div>
-<br><br><br><br><br>
-<!-- Footer -->
-<footer class="colorFooter">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text"> <strong>Institucion Educativa Central</strong></h5>
-        <p class="grey-text text-lighten-4">Saldaña - Tolima</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text"><strong>Redes Sociales</strong></h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="#" target="_blank">Facebook</a></li>
-          <li><a class="grey-text text-lighten-3" href="#">Twitter</a></li>
-          <li><a class="grey-text text-lighten-3" href="#">Instagram</a></li>
-        </ul>
-      </div>
+        <?php endif; ?>
+      </form>
     </div>
   </div>
-  <div class="footer-copyright">
+  <!-- Footer -->
+  <footer class="colorFooter">
     <div class="container">
-    <p class="copy">Copyright © - 2024 Alfredo Gomez Culma Derechos Reservados</p>
-    <a> <img src="views/imagenes/logoIECentral-removebg.png" width="100px" height="90px" class="logoAbajo right"></a>
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text"> <strong>Institución Educativa Central</strong></h5>
+          <p class="grey-text text-lighten-4">Saldaña - Tolima</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text"><strong>Redes Sociales</strong></h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="#" target="_blank">Facebook</a></li>
+            <li><a class="grey-text text-lighten-3" href="#">Twitter</a></li>
+            <li><a class="grey-text text-lighten-3" href="#">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</footer>
+    <div class="footer-copyright">
+      <div class="container">
+        <p class="copy">Copyright © 2024 Alfredo Gomez Culma y el ICFES</p>
+        <a> <img src="views/imagenes/logoIECentral-removebg.png" width="100px" height="90px" class="logoAbajo right"></a>
+      </div>
+    </div>
+  </footer>
 
-<!--reCaptchat-->
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<!-- Framework: Materialize -->
-<script type="text/javascript" src="views/materialize/js/materialize.min.js"></script>
-<!-- Libreria: Lottie -->
-<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<!-- Links iconos font-awesome -->
-<script src="https://kit.fontawesome.com/3f592185f1.js" crossorigin="anonymous"></script>
-<!-- Script para el registrarView -->
-<script src="views/js/jsRegistrar.js"></script>
-<!-- Archivo js de alertas -->
-<script src="views/js/sweetAlert.js"></script>
+  <!--reCaptchat-->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <!-- Framework: Materialize -->
+  <script type="text/javascript" src="views/materialize/js/materialize.min.js"></script>
+  <!-- Libreria: Lottie -->
+  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+  <!-- Links iconos font-awesome -->
+  <script src="https://kit.fontawesome.com/3f592185f1.js" crossorigin="anonymous"></script>
+  <!-- Script para el registrarView -->
+  <script src="views/js/jsRegistrar.js"></script>
+  <!-- Archivo js de alertas -->
+  <script src="views/js/sweetAlert.js"></script>
 </body>
+
 </html>
